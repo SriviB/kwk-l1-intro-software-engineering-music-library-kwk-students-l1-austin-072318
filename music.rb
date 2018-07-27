eeb2f6f0116b90e3f8d1b368c389c36b69669534
SriviB <srivibalaji33@gmail.com>
2018-07-27 16:45:49 +0000

    automatically backed up by learn

--- music.rb
+++ music.rb
@@ -21,7 +21,7 @@ end
 #This method should take two arguments, the music library hash, and the artist. Remember, keys in hashes are symbols, so you'll need to expect the method to accept the artist as a symbol. 
 
 def delete_artist(library = {}, library[artist])
-  artist = :artist
+  artist = library[artist]
   new_lib = library.delete(artist)
   print new_lib
 end
